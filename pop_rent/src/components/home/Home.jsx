dee1e1f8dece7a38ffb6d8b78b0abce18d1f3026
dinesh07sengar <sengardante@gmail.com>
2023-09-26 11:42:14 +0530

    first

---
+++ pop_rent/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import { Box, Grid, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+        <Box>
+            <Heading>Search Properties for reat</Heading>
+            <Grid></Grid>
+
+        </Box>
+    </div>
+  )
+}
---
+++ pop_rent/src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Box, Button, Flex, Heading, Spacer, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <Box bg={"#CCCCCC"} p={"1% 7%"}>
+        <Flex>
+            <Box display={"flex"} gap={"12"}>
+                <Heading my={"auto"} size={"lg"} >LOGO</Heading>
+                <Text my={"auto"} fontWeight={"medium"} fontSize={"large"}>Home</Text>
+            </Box>
+            <Spacer/>
+            <Box display={"flex"} gap={"8"}>
+                <Button size={"md"} w={"110px"} bg={"#A2DEED"} borderRadius={"10px"}>Login</Button>
+                <Button w={"110px"} bg={"#D1EFF7"} borderRadius={"10px"}>Signup</Button>
+            </Box>
+        </Flex>
+    </Box>
+  )
+}
